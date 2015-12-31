95c3387569905473ecd76e0726603afff5458f1d
zhangqq <damonzh@126.com>
2015-12-31 16:08:17 +0800

    regex

---
+++ Java/regex/RegexMatches.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher ;
+import java.util.regex.Pattern ;
+
+public class RegexMatches{
+	public static void main(String args[]){
+		//按指定模式在字符串查找
+		String line = "This order was places for QT3000!";
+		String pattern = "(.*)(\\d+)(.*)";
+		
+		//创建Pattern对象
+		Pattern r = Pattern.compile(pattern);
+		
+		//现在创建matcher对象
+		Matcher m = r.matcher(line);
+		if(m.find()){
+			System.out.println("Found value:"+m.group(0));
+			System.out.println("Found value:"+m.group(1));
+			System.out.println("Found value:"+m.group(2));
+		}else{
+			System.out.println("NO Match!");
+		}
+	}
+}
---
+++ Java/regex/regex.md
@@ -0,0 +1,13 @@
+###Java正则表达式
+正则表达式定义了字符串的模式
+
+正则表达式可以用来搜索、编辑或处理文本
+
+正则表达式并不仅限于一种语言
+
+Java正则表达式java.util.regex包含主要三个类：
+- Pattern类：编译表示
+- Matcher类：Matcher对象是对输入字符串进行解释和匹配操作的引擎。
+- PatternSynctaxException：
+
+Pattern和Matcher都没有公共的构造方法。
